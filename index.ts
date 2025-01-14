82d2ecf6c7b1b80a7ff6e315535f562bc6cc1d7f
crypto-services <hossein.khoshtaghaza@republiccrypto.com>
2025-01-14 18:52:44 +0800

    Check if claimed

--- index.ts
+++ index.ts
@@ -1,45 +1,19 @@
 import { Web3 } from 'web3'
 import cron from 'node-cron'
 
+import abi from './abi.json'
+
 require('dotenv').config()
 
 const web3 = new Web3(process.env.EVM_RPC_URL)
+const contract = new web3.eth.Contract(abi, process.env.DISTRIBUTION_CONTRACT)
 
 let startHeight = 0
-
-async function scanBlocks() {
-  try {
-    const currentHeight = await web3.eth.getBlockNumber()
-    const endHeight = Number(currentHeight) - 100
-    if (!startHeight) {
-      startHeight = Number(currentHeight) - Number(process.env.LOOK_BACK)
-    }
-    console.log(`Scanning from ${startHeight} to ${endHeight}`)
-
-    for (let i = startHeight; i < endHeight; i++) {
-      const block = await web3.eth.getBlock(i)
-      if (block.miner === process.env.TARGET_COINBASE.toLowerCase()) {
-        const isValid = await verifyBlock(Number(block.number), block.hash)
-        if (isValid) {
-          console.log(
-            `Found block mined by target: ${block.number} with timestamp ${block.timestamp}`,
-          )
-          const proof = await fetchProof(Number(block.timestamp))
-          console.log(proof)
-        } else {
-          console.log(`Block is no longer in chain: ${Number(block.number)} ${block.hash}`)
-        }
-      }
-    }
-  } catch (e) {
-    console.log(e)
-  }
-}
 
 async function verifyBlock(number: number, hash: string): Promise<boolean> {
   try {
     const parent = await web3.eth.getBlock(number + 1)
-    if (parent.parentHash == hash) {
+    if (parent.parentHash === hash) {
       return true
     } else {
       return false
@@ -68,6 +42,54 @@ async function fetchProof(timestamp: number) {
   }
 }
 
-cron.schedule('*/10 * * * *', async () => {
+async function canClaim(timestamp: number): Promise<boolean> {
+  try {
+    return await contract.methods.isTimestampActionable(timestamp).call()
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+async function getTargetHeight(): Promise<number> {
+  try {
+    const currentHeight = await web3.eth.getBlockNumber()
+    const endHeight = Number(currentHeight) - 100
+    if (!startHeight) {
+      startHeight = Number(currentHeight) - Number(process.env.LOOK_BACK)
+    }
+    console.log(`Scanning from ${startHeight} to ${endHeight}`)
+    return endHeight
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+async function scanBlocks() {
+  try {
+    const endHeight = await getTargetHeight()
+    for (let i = startHeight; i < endHeight; i++) {
+      const block = await web3.eth.getBlock(i)
+      if (block.miner === process.env.TARGET_COINBASE.toLowerCase()) {
+        const isValid = await verifyBlock(Number(block.number), block.hash)
+        if (isValid) {
+          const claimable = await canClaim(Number(block.timestamp))
+          if (claimable) {
+            const proof = await fetchProof(Number(block.timestamp))
+            console.log(proof)
+          } else {
+            console.log(`Block already claimed: ${block.number}`)
+          }
+        } else {
+          console.log(`Block is no longer in chain: ${Number(block.number)} ${block.hash}`)
+        }
+      }
+    }
+    startHeight = endHeight
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+cron.schedule('*/5 * * * *', async () => {
   scanBlocks()
 })
