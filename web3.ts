48f769d0722b7d1cc0a6339ae9a4b93dd81d09cd
crypto-services <hossein.khoshtaghaza@republiccrypto.com>
2025-01-15 09:49:36 +0800

    Reformat

--- web3.ts
+++ web3.ts
@@ -1,6 +1,8 @@
 import { Web3 } from 'web3'
 import abi from './abi.json'
 
+require('dotenv').config()
+
 const web3 = new Web3(process.env.EVM_RPC_URL)
 const signer = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
 web3.eth.accounts.wallet.add(signer)
